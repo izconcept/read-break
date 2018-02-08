2179164682a102954988f386eec9c55cf79b314f
Kevin Zhang <izkevin52@gmail.com>
2018-02-07 22:29:40 -0500

    updated manifest + identifying page

--- background.js
+++ background.js
@@ -1,3 +1,22 @@
 // Inject javascript code into page here
 
-console.log('background')
+if($('article').text()) {
+	article = $('article').text();
+
+	var host = window.location.hostname
+
+	switch(host) {
+	case "www.newyorker.com":
+		console.log('The New Yorker');
+		break;
+	case "www.economist.com":
+		console.log('The Economist');
+		break;
+	case "www.theatlantic.com":
+		console.log('The Atlantic');
+		break;
+	case "www.washingtonpost.com":
+		console.log('The Washington Post');
+		break;
+	}
+}
