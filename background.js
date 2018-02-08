bc12ccb87d44dddc036cdf63d2c3e9805737c63f
Kevin Zhang <izkevin52@gmail.com>
2018-02-07 23:07:06 -0500

    fixed fatal class import injection

--- background.js
+++ background.js
@@ -20,23 +20,22 @@ $(document).ready(function() {
 			switch(host) {
 			case "www.newyorker.com":
 				console.log('The New Yorker');
-				article.prepend("<span class="readTime">Estimated Read Time: " + estimatedReadTime + " Minutes</span>")
+				article.prepend("<span class='readTime'>Estimated Read Time: " + estimatedReadTime + " Minutes</span>")
 				break;
 			case "www.economist.com":
 				console.log('The Economist');
-				article.prepend("<span class="readTime">Estimated Read Time: " + estimatedReadTime + " Minutes</span>")
+				article.prepend("<span class='readTime'>Estimated Read Time: " + estimatedReadTime + " Minutes</span>")
 				break;
 			case "www.theatlantic.com":
 				console.log('The Atlantic');
-				article.find('.article-cover-content-wrapper').prepend("<span class="readTime" >Estimated Read Time: " + estimatedReadTime + " Minutes</span>")
+				article.find('.article-cover-content-wrapper').prepend("<span class='readTime' >Estimated Read Time: " + estimatedReadTime + " Minutes</span>")
 				break;
 			case "www.washingtonpost.com":
 				console.log('The Washington Post');
-				article.prepend("<span class="readTime">Estimated Read Time: " + estimatedReadTime + " Minutes</span>")
+				article.prepend("<span class='readTime'>Estimated Read Time: " + estimatedReadTime + " Minutes</span>")
 				break;
 			}
 
-			console.log("Estimated Read Time: " + estimatedReadTime + " Minutes");
 		}
 	}, 800)
 	
