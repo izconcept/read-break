b048fd69769466730ecd830174b05d7cf47cbe9c
Kevin Zhang <izkevin52@gmail.com>
2018-02-12 13:47:02 -0500

    put injected HTML into js variable

--- background.js
+++ background.js
@@ -14,29 +14,31 @@ $(document).ready(function() {
 
 			var words = articleText.split(' ').length;
 
-			estimatedReadTime = Math.round(words/WPM);
+			var estimatedReadTime = Math.round(words/WPM);
+
+			var read_time_div = "<div class='read-div'><span class='readTime'>Estimated Read Time: " + estimatedReadTime + " Minutes</span></div>"
 
 
 			switch(host) {
 			case "www.newyorker.com":
 				console.log('The New Yorker');
-				article.prepend("<span class='readTime'>Estimated Read Time: " + estimatedReadTime + " Minutes</span>")
+				article.prepend(read_time_div)
 				break;
 			case "www.economist.com":
 				console.log('The Economist');
-				article.prepend("<span class='readTime'>Estimated Read Time: " + estimatedReadTime + " Minutes</span>")
+				article.prepend(read_time_div)
 				break;
 			case "www.theatlantic.com":
 				console.log('The Atlantic');
-				article.find('.article-cover-content-wrapper').prepend("<span class='readTime' >Estimated Read Time: " + estimatedReadTime + " Minutes</span>")
+				article.find('.article-cover-content-wrapper').prepend(read_time_div)
 				break;
 			case "www.washingtonpost.com":
 				console.log('The Washington Post');
-				article.prepend("<span class='readTime'>Estimated Read Time: " + estimatedReadTime + " Minutes</span>")
+				article.prepend(read_time_div)
 				break;
 			case "www.nytimes.com":
 				console.log('The New York Times');
-				article.find('header').prepend("<span class='readTime'>Estimated Read Time: " + estimatedReadTime + " Minutes</span>")
+				article.find('header').prepend(read_time_div)
 				break;
 			}
 
